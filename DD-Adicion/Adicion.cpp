0574ecafdbb38a2a05ef5e33d3528d8f3f3df5ed
vhbu <49312314+vhbu@users.noreply.github.com>
2019-04-17 10:14:53 -0300

    Add files via upload

---
+++ DD-Adicion/Adicion.cpp
@@ -0,0 +1,36 @@
+/*
+
+Bulgarelli victor
+
+Programa Adición
+
+V1 - 16-04-2019
+*/
+
+#include <iostream>
+#include<stdio.h>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+ int A,B,C;
+
+    cout<<endl;
+        cout<< "Suma de dos valores A Y B" << endl;
+    cout<<endl;
+
+    cout<<endl;
+        cout<< "Ingresar valor A: ";
+            cin >>A;
+    cout<<endl;
+        cout<< "Ingresar valor B: ";
+            cin >>B;
+    cout<<endl;
+        C=A+B;
+            cout<<"El resultado de sumar "<<A<<" y "<<B<<" es "<<C;
+    cout<<endl;
+
+    return 0;
+}
